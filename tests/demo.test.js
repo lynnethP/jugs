6143c1cd787d39584d1940a7c816f9d6fea68fd5
lynnethP <lynnethpereira12@gmail.com>
2024-01-28 21:00:54 -0400

    First Test

---
+++ tests/demo.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Jugs from "../src/Jugs";
+
+describe('Test <Jugs />', () => {
+    test('Render the Jugs component', () => {
+        const {container} = render('<Jugs />');
+        expect(container).toMatchSnapshot();
+    });
+});
